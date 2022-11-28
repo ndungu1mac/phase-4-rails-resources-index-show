8d075a7790e251ed1fe51d542f97570be20bd11a
ndungu1mac <macgeorgendungu@gmail.com>
2022-11-28 23:20:07 +0300

    Done

---
+++ db/schema.rb
@@ -0,0 +1,9 @@
+
+  create_table "birds", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
